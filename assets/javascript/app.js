e87449ca3cccb11a800a742fca5775e86602b6ad
Louise Miller <louise.austex@gmail.com>
2016-06-14 17:59:57 -0500

    add distance matrix service call and display result

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,12 +10,12 @@ function initMap() {
 	var styles = [
 		{featureType: 'water',
 		 stylers: [
-		 	{color: '#fff'}
+		 	{color: '#000000'}
 		 ]},
 		 {featureType: 'administrative',
 		  elementType: 'labels.text.stroke',
 		 stylers: [
-		 	{color: '#fff'},
+		 	{color: '#ffffff'},
 		 	{weight: 8}
 		 ]},
 		 {featureType: 'transit.station',
@@ -34,21 +34,35 @@ function initMap() {
 		styles: styles,
 		mapTypeControl: false
 	});
+	
 	// this is the location for the marker
 	// you can make this an array of marker objects
+	// or a single location as follows:
 	//var tribeca = {lat: 40.719526, lng: -74.0089934};
+	//
 	var locations = [
 		{title: 'Park Ave Penthouse', location: {lat: 30.2600, lng: -97.7400}},
 		{title: 'Chelsea Loft', location: {lat: 30.5555, lng: -97.8888}}
 
 	];
 
+	// this is the pop-up window that appears when you
+	// click on a marker
 	var largeInfowindow = new google.maps.InfoWindow();
 
-	//style the markers
+	//style the markers.  we use highlightedIcon
+	//when user hovers over the marker
 	var defaultIcon = makeMarkerIcon('0091ff');
 	var highlightedIcon = makeMarkerIcon('FFFF24');
 
+	// this loop goes through the locations array
+	// and creates a marker for each location
+	// then pushes the marker information into an array
+	//  Creates listeners for each marker
+	//  so the infowindow will open when user
+	//  clicks on the marker, and marker will highlight when
+	//  mouseover, then return to normal on mouseout
+	//
 	for (var i = 0; i<locations.length; i++) {
 		// get the position from the locations array
 		var position = locations[i].location;
@@ -76,7 +90,8 @@ function initMap() {
 		});
 	}
 
-
+	// function for populating the infowindow that appears when 
+	// user clicks on marker
 
 	function populateInfoWindow(marker, infowindow) {
 		// check to make sure the infowindow is not already opened in this marker.
@@ -91,7 +106,9 @@ function initMap() {
 		}
 	}
 
-
+	// function for stylizing the markers.  Not sure I understand
+	// the call yet.  Need to look into this further.
+	//
 	function makeMarkerIcon(markerColor){
 		var markerImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_spin&chld=1.15|0|"+ markerColor + "|40|_|%E2%80%A2",
 			new google.maps.Size(21,34),
@@ -100,6 +117,9 @@ function initMap() {
 			new google.maps.Size(21,34));
 		return markerImage;
 	};
+}  //end of initMap function
+
+// listener for user clicking on "show listing" button
 
 $('#show-listing').on("click", function(event){
 	event.preventDefault();
@@ -113,6 +133,8 @@ $('#show-listing').on("click", function(event){
 	map.fitBounds(bounds);
 });
 
+// listener for user clicking on "hide listing" button
+
 $('#hide-listing').on("click", function(event){
 	event.preventDefault();
 	for (var i=0; i < markers.length; i++) {
@@ -121,9 +143,49 @@ $('#hide-listing').on("click", function(event){
 });
 
 
-
-};
-
 $(document).ready(function(){
 	console.log ("hello");
-})
+//CODE FOR DISTANCE MATRIX SERVICE
+var origin1 = new google.maps.LatLng(30.2600, -97.7400);
+var origin2 = new google.maps.LatLng(30.2700, -97.7440);
+var origin3 = new google.maps.LatLng(30.2650, -97.7300);
+var destinationA = new google.maps.LatLng( 30.2615, -97.7410);
+var destinationB = new google.maps.LatLng( 30.2627, -97.7450);
+var destinationC = new google.maps.LatLng( 30.2613, -97.7510);
+
+var service = new google.maps.DistanceMatrixService();
+service.getDistanceMatrix(
+	{ 
+	origins: [origin1, origin2, origin3],
+	destinations: [destinationA, destinationB, destinationC],
+	travelMode: google.maps.TravelMode.DRIVING,
+	// transitOptions: TransitOptions,
+	// drivingOptions: DrivingOptions,
+	// unitSystem: UnitSystem,
+	// avoidHighways: Boolean,
+	// avoidTolls: Boolean,
+	}, calcMatrix);
+
+function calcMatrix (response, status){
+	if (status == google.maps.DistanceMatrixStatus.OK) {
+		var origins = response.originAddresses;
+		var destinations = response.destinationAddresses;
+
+		for (var i=0; i < origins.length; i++) {
+			var results = response.rows[i].elements;
+			for (var j=0; j < results.length; j++) {
+				var element = results[j];
+				var distance = element.distance.text;
+				var duration = element.duration.text;
+				var from = origins[i];
+				var to = destinations[j];
+				$('#result').append("origin: " + from+"<br>");
+				$('#result').append("destination: " + to+"<br>");
+				$('#result').append("distance: "+distance+"<br>");
+				$('#result').append("duration: "+duration+"<br>");
+			}
+		}
+	}
+};
+
+});
