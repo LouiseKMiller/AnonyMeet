44b3267438105416c5ec369f68322a15cee66f96
Hector <you@example.com>
2016-06-18 03:38:40 -0500

    Adding files

--- assets/javascript/midway.js
+++ assets/javascript/midway.js
@@ -3,8 +3,23 @@ var geocoder;
 var initialAddressLatLong = {lat: 30.267, lng: -97.744};
 var address1;
 var address2;
-var addres1LatLng;
-var addres2LatLng;
+var address1LatLng;
+var address2LatLng;
+
+var latLngPersonA;
+var latLngPersonB;
+
+//adding firebase to appMeet.js
+var fbPeople = new Firebase("https://anonymeetut.firebaseio.com/people");
+
+var fbPersonA = fbPeople.child('/personA');
+var fbPersonB = fbPeople.child('/personB');
+
+var personAAddress;
+var personBAddress;
+
+// location for midpoint in lat lng
+var midpoint;
 
 function initialize() {
     geocoder = new google.maps.Geocoder();
@@ -18,52 +33,78 @@ function initialize() {
         address1 = document.getElementById('address1').value;
         address2 = document.getElementById('address2').value;
 
-        console.log(address1, address2);
-
-        // geocodeAddress(address1);
-        getLatLongFromAddress(address1, address2);
-        console.log(addres1LatLng, addres2LatLng);
-        calculateMidPoint(address1, address2);
-    });
-}
-
-function plotMidPoint(address) {
-    // var address = document.getElementById('address1').value;
-    geocoder.geocode({'address': address}, function (results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-            map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
+        getAddresFirebase(fbPersonA)
+            .then(function (addressA) {
+                console.log(addressA);
+                personAAddress = addressA;
+                return getAddresFirebase(fbPersonB)
+            })
+            .then(function (addressB) {
+                console.log(addressB);
+                personBAddress = addressB;
+            })
+            .catch(function (error) {
+                console.log(error);
             });
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
-    });
-}
 
-function getLatLongFromAddress(address1, address2) {
-    geocoder.geocode({'address': address1}, function (results) {
-        addres1LatLng = {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()};
-        console.log(addres1LatLng);
-    });
-
-    geocoder.geocode({'address': address2}, function (results) {
-        addres2LatLng = {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()};
-        console.log(addres2LatLng);
-    });
-}
-
-function calculateMidPoint (address1, address2) {
-    geocoder.geocode({'address': address}, function (results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-            map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
+        getLatlngFromAddress(address1)
+            .then(function (latlng) {
+                address1LatLng = latlng;
+                return getLatlngFromAddress(address2);
+            })
+            .then(function (latlng) {
+                address2LatLng = latlng;
+                // return 'finally';
+            })
+            .then(function (status) {
+                return calculateMidPoint(address1LatLng, address2LatLng);
+            })
+            .then(function (midPoint) {
+                // console.log(midPoint, 'midpoint')
+                midpoint = midPoint;
+            })
+            .catch(function (result) {
+                console.log(result);
             });
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
     });
+}
+
+function getAddresFirebase(url) {
+    return new Promise(function(resolve, reject) {
+        url.on("value", function (snapshot) {
+            // console.log(snapshot.val(), 'gAGGA');
+            if (snapshot != null) {
+                // console.log(snapshot.val(), 'gAGGA');
+                var streetNumber = snapshot.val().streetNumber;
+                var street = snapshot.val().street;
+                var city = snapshot.val().city;
+                var state = 'TX';
+                var zip = snapshot.val().zip;
+
+                var completeAddress = streetNumber + ' ' + street + ' ' + city + ', ' + state + ' ' + zip;
+                // console.log(completeAddress);
+
+                resolve(completeAddress);
+            } else {
+                reject('ERRORERROR EROEROER');
+            }
+        });
+    });
+}
+
+function getLatlngFromAddress(address) {
+    return new Promise(function(resolve, reject) {
+        geocoder.geocode({ 'address': address }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                // console.log(results[0].geometry.location);
+                resolve(results[0].geometry.location);
+            } else {
+                reject(status);
+            }
+        });
+    });
+}
+
+function calculateMidPoint (a1, a2) {
+    return google.maps.geometry.spherical.interpolate(a1, a2, .5);
 }
